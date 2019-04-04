e9c9f2d3e8a37051da9a5594a41929c982a197f2
Chris Symonds <c.c.symonds@leeds.ac.uk>
2019-04-04 15:41:30 +0100

    step 1 of practical exercises

---
+++ ModernFortran/Day1_Afternoon/Notes.md
@@ -0,0 +1,3 @@
+# Notes for second session of the NAG Fortran Modernisation Workshop
+
+##### 4th April 2019 at UMIST
---
+++ ModernFortran/fmw_exercises/src/types_mod.f90
@@ -0,0 +1,15 @@
+module types_mod
+  use, intrinsic :: iso_fortran_env
+
+  implicit none
+  ! everything is private unless otherwise stated
+  private
+  public :: SP
+
+  integer, parameter :: SP = REAL32
+  integer, parameter :: DP = REAL64
+  integer, parameter :: SI = INT32
+  integer, parameter :: DI = INT64
+contains
+
+end module types_mod
